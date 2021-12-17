d52d5f171dc62bc80ff6a090bc0ad9b61a11a283
Slava Kozlov <v.kozlov@freshlimesoft.com>
2021-12-17 21:01:54 +0300

    commit in first branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Profile/>
       </header>
     </div>
   );
