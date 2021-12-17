d823e188db8376b50fd8cb55bd4633abebf58a61
Slava Kozlov <v.kozlov@freshlimesoft.com>
2021-12-17 20:54:32 +0300

    commit after changes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Trial project</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Slava Kozlov</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Git
         </a>
       </header>
     </div>
